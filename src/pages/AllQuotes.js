6fbeb48032d56fc38557a903c63dea176e447a7c
abbygoyal <abhishekgoyal3669@gmail.com>
2023-01-26 11:20:17 +0530

    updates

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,4 +1,9 @@
+import QuoteList from "../components/quotes/QuoteList";
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Abby", text: "Learning React is Fun!!" },
+  { id: "q2", author: "Abhishek", text: "Learning React is Fun!!" },
+];
 const AllQuotes = () => {
-  return <h1>All Quotes Page!</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 export default AllQuotes;
